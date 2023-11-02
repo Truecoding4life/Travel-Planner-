10352138ccd61ab7ca17dde2beea76b93bbb138b
Thai Nghiem <thainghiem@Thais-MacBook-Air.local>
2023-11-02 11:31:14 -0700

    added location model

--- Develop/Models/Location.js
+++ Develop/Models/Location.js
@@ -1,10 +1,27 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require ('../../config/connect');
 
-class Traveler extends Model{};
+class Location extends Model{};
 
-Traveler.init(
+Location.init(
     {
-        
-    }
-)
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        }, 
+        name: {
+            type: DataTypes.STRING,
+            validate: {
+                isAlphanumeric: true,
+              },
+        },
+    }, 
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,  // TABLE NAME WON'T PLURALIZE
+      underscored: false,     // AUTOMATIC UNDERSCORE TABLE NAME
+      modelName: 'location',      // MODEL NAME
+    },
+);
